5678b1668ad570e0e18157e5ec5ccbf6cee7663a
lucaspouchot <lucas.pouchot@digitalairways.com>
2018-03-08 16:54:30 +0100

    update

--- go.js
+++ go.js
@@ -1,3 +1,28 @@
+/**
+
+Modified for the Thingbox
+Digital Airways 2015
+
+
+**/
+
+
+/**
+ * Copyright 2013, 2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 module.exports = function(RED) {
     "use strict";
     function main(config) {
@@ -10,19 +35,4 @@ module.exports = function(RED) {
         });
     }
 	RED.nodes.registerType("go",main);
-	
-    RED.httpAdmin.post("/inject/:id", RED.auth.needsPermission("inject.write"), function(req,res) {
-        var node = RED.nodes.getNode(req.params.id);
-        if (node != null) {
-            try {
-                node.receive();
-                res.send(200);
-            } catch(err) {
-                res.send(500);
-                node.error("Inject failed:"+err);
-            }
-        } else {
-            res.send(404);
-        }
-    });
 }
---
+++ info.md
@@ -0,0 +1,5 @@
+# go node
+
+
+Forked and modified from node-RED input node.
+This node sends an input to the following nodes with a blank payload.
